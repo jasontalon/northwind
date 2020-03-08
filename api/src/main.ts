5c5f2c6beb4895def689724c336daf88e25fb6d1
Jason Talon <jason.talon@outlook.com>
2020-03-08 20:39:03 +0800

    traefik support

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,6 +6,6 @@ require("dotenv").config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new LoggingInterceptor());
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000/*, '0.0.0.0'*/);
 }
 bootstrap();
